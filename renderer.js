0a0b6c9b025118f7ccb96137998d09c20da56dec
Jaybee18 <mister.jaybee18@gmail.com>
2022-07-03 21:26:49 +0200

    added scrollbars, fixed header, added more dynamic feeling added some more widgets to the header

--- renderer.js
+++ renderer.js
@@ -10,23 +10,73 @@ document.getElementById("bpm_sub_button").addEventListener('click', () => {
     label.innerHTML = Number.parseInt(label.innerHTML) - 1;
 });
 
-// track canvas
-(function() {
-    var c = document.getElementById("track_canvas");
-    var ctx = c.getContext("2d");
-    ctx.translate(0.5, 0.5);
+// add track-button functionality
+var resizing_track = null;
+document.getElementById("track_add").addEventListener('click', () => {
+  // clone and spawn element
+  var template = document.getElementById("track_template");
+  var clone = template.content.cloneNode(true);
+  var tracks = document.getElementById("tracks");
+  tracks.insertBefore(clone, document.getElementById("track_add"));
 
-    ctx.fillStyle = 'rgb(58, 74, 84)';
-    for (let i = 0; i < 100; i+=8) {
-        ctx.fillRect(i*20, 0, 20*4, 500);
+  // draw canvas
+  var element = tracks.children[tracks.childElementCount-2];
+  var c = element.querySelector("#track_canvas");
+  var ctx = c.getContext("2d");
+  ctx.fillStyle = 'rgb(58, 74, 84)';
+  for (let i = 0; i < 100; i+=8) {
+      ctx.fillRect(i*20, 0, 20*4, 500);
+  }
+  ctx.strokeStyle = 'rgb(0, 0, 0, 0.3)';
+  for (let i = 0; i < 100; i++) {
+      ctx.moveTo(i*20, 0);
+      ctx.lineTo(i*20, 500);
+  }
+  ctx.stroke();
+
+  // add resizing functionality
+  var resize_handle = element.querySelector("#track_resize");
+  resize_handle.onmousedown = function(e) {
+    resizing_track = element;
+    return false;
+  };
+
+  document.getElementById("tracks").onmousemove = function(e) {
+    if (resizing_track === null) {
+      return false;
     }
-    ctx.strokeStyle = 'rgb(0, 0, 0, 0.3)';
-    for (let i = 0; i < 100; i++) {
-        ctx.moveTo(i*20, 0);
-        ctx.lineTo(i*20, 500);
+
+    var new_height = e.clientY - resizing_track.offsetTop;
+    resizing_track.style.height = new_height + "px";
+  };
+
+  document.onmouseup = function(e) {
+    resizing_track = null;
+  };
+});
+
+// add slider functionality
+var handling_slider = null;
+document.querySelectorAll(".slider").forEach(slider => {
+  slider.querySelector(".handle").onmousedown = function(e) {
+    handling_slider = slider;
+    return false;
+  };
+  document.addEventListener("mouseup", () => {
+    handling_slider = null;
+  });
+  document.addEventListener("mousemove", (e) => {
+    if (handling_slider === null) {
+      return;
     }
-    ctx.stroke();
-})();
+  
+    var handle = handling_slider.querySelector(".handle");
+    if (e.clientY < 0 || e.clientY > handling_slider.clientHeight - handle.clientHeight + 1) {
+      return;
+    }
+    handle.style.margin = e.clientY + "px 0px 0px 0px";
+  });
+});
 
 // header buttons
 document.querySelectorAll(".header_button").forEach(button => {
@@ -51,25 +101,9 @@ document.querySelectorAll(".header_button").forEach(button => {
     icon.style.transform = 'scale(1.1)'; 
   });
 });
-/*
-document.getElementById("button").classList.add("unclicked");
-document.getElementById("button").addEventListener('click', () => {
-  var button = document.getElementById("button");
-  var icon = document.querySelector("#button > i");
-  if (button.classList.contains("clicked")){
-    button.classList.remove("clicked");
-    button.classList.add("unclicked");
-    icon.style.color = 'rgb(225, 225, 225)';
-  } else {
-    button.classList.remove("unclicked");
-    button.classList.add("clicked");
-    icon.style.color = 'rgb(80, 19, 0)';
-  }
-});*/
 
 // sidebar resizing 
 var resizing_sidebar = false;
-var resizing_track = false;
 
 (function() {
   var container = document.getElementById("content"),
@@ -99,34 +133,5 @@ var resizing_track = false;
   document.onmouseup = function(e) {
     // stop resizing
     resizing_sidebar = false;
-  }
-})();
-(function() {
-  var container = document.getElementById("tracks"),
-    content = document.getElementById("track_wrap"),
-    handle = document.getElementById("track_resize");
-
-  handle.onmousedown = function(e) {
-    resizing_track = true;
-    return false;
-  };
-
-  document.getElementById("main_content").onmousemove = function(e) {
-    // we don't want to do anything if we aren't resizing.
-    if (!resizing_track) {
-      return;
-    }
-
-    var offsetTop = container.clientHeight - (e.clientY - container.offsetTop);
-
-    content.style.height = container.clientHeight - offsetTop + "px";
-    //right.style.height = offsetTop + "px";
-
-    return false;
-  }
-
-  document.getElementById("main_content").onmouseup = function(e) {
-    // stop resizing
-    resizing_track = false;
   }
 })();
