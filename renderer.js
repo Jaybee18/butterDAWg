7a513ef6dd2a96be7616926b5dcfd14830f2f455
Jaybee18 <mister.jaybee18@gmail.com>
2022-07-04 23:37:47 +0200

    added full track-object move functionality

--- renderer.js
+++ renderer.js
@@ -2,6 +2,7 @@
 
 // add track-button functionality
 var resizing_track = null;
+var tracklist = [];
 document.getElementById("track_add_label").addEventListener('click', () => {
   // clone and spawn element
   var template = document.getElementById("track_template");
@@ -11,6 +12,7 @@ document.getElementById("track_add_label").addEventListener('click', () => {
 
   // draw canvas
   var element = tracks.children[tracks.childElementCount-2];
+  tracklist.push(element);
   var c = element.querySelector("#track_canvas");
   var ctx = c.getContext("2d");
   ctx.fillStyle = 'rgb(58, 74, 84)';
@@ -40,10 +42,99 @@ document.getElementById("track_add_label").addEventListener('click', () => {
     resizing_track.style.height = new_height + "px";
   };
 
-  document.onmouseup = function(e) {
-    resizing_track = null;
-  };
+  // drag and drop preview selection
+  element.addEventListener("mouseenter", () => {
+    currently_hoverd_track = element;
+  });
+
+  // add drag and drop functionality (temporarily misused for the test widget)
+  dragElement(element.querySelector(".track_object"));
 });
+
+document.addEventListener("mouseup", () => {
+  resizing_track = null;
+});
+
+document.getElementById("tracks").addEventListener("mouseleave", () => {
+  // no track is hovered, when the mouse leaves the tracks-area
+  currently_hoverd_track = null;
+});
+
+// function for dragging samples
+// from https://www.w3schools.com/howto/howto_js_draggable.asp
+var xsnap = 20;
+function dragElement(elmnt) {
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  if (document.getElementById(elmnt.id + "header")) {
+    // if present, the header is where you move the DIV from:
+    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+  } else {
+    // otherwise, move the DIV from anywhere inside the DIV:
+    elmnt.onmousedown = dragMouseDown;
+  }
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.addEventListener("mouseup", closeDragElement);
+    // call a function whenever the cursor moves:
+    document.addEventListener("mousemove", elementDrag);
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+
+    // determine the track the current object should be a child of
+    t = currently_hovered_track();
+    if (t !== null) {
+      // snap to currently hovered track, if present
+      var content = t.querySelector(".track_content");
+      content.appendChild(elmnt);
+    } else {return;}
+
+    // calculate the new cursor position:
+    var mouseX = e.clientX;
+    pos1 = pos3 - mouseX;
+    if (Math.abs(pos1) < xsnap) {
+      return;
+    }
+    pos1 = pos1 - pos1%xsnap;
+
+    pos4 = e.clientY;
+    if (e.clientX < content.getBoundingClientRect().left) {
+      pos3 = content.getBoundingClientRect().left;
+    } else {
+      pos3 = e.clientX;
+    }
+    // set the element's new position:
+    var newX = (elmnt.offsetLeft - pos1);
+    if (newX < 0) {
+      newX = 0;
+    }
+    elmnt.style.top = "0px";
+    elmnt.style.left = newX + "px";
+  }
+
+  function closeDragElement() {
+    // stop moving when mouse button is released:
+    document.removeEventListener("mouseup", closeDragElement);
+    document.removeEventListener("mousemove", elementDrag);
+  }
+}
+
+function currently_hovered_track() {
+  t = null;
+  tracklist.forEach(track => {
+    if (track.matches(":hover")) {
+      t = track;
+    }
+  });
+  return t;
+}
 
 // add slider functionality
 var handling_slider = null;
